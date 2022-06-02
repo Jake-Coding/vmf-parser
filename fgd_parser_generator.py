fda297069bc3f67b8dc1152a7fcfab454143f2cb
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2022-06-02 14:23:03 -0600

    redo again :)) begin parser for FGD files. Currently working for baseclass without any flags or choices. Here we go again. First commit of this I suppose

---
+++ fgd_parser_generator.py
@@ -0,0 +1,12 @@
+
+import lark
+def get_text(filename : str) ->str:
+    text = ""
+    with open(filename, "r") as f:
+        text = f.read()
+    return text
+parser = lark.Lark(get_text("./parser_generator.lark"), start="fgd", debug=True)
+
+print(parser)
+parsed = parser.parse(get_text("./not_an_fgd.fgd"))
+print(parsed.pretty())
